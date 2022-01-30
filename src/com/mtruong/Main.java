f9d26c4e39c5399c777f41293673cc09e7c492e3
Michael Truong <truong.michael.2013@gmail.com>
2022-01-30 16:02:20 -0500

    First Commit

---
+++ src/com/mtruong/Main.java
@@ -0,0 +1,9 @@
+package com.mtruong;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("\n Practice using Intellij IDEA with Java Development \n");
+    }
+}
